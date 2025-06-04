36b59b07bd8f9363750e9eb1dc9b554663e8c989
mishkanchik <csprince666@gmail.com>
2025-06-04 17:30:21 +0300

    tteh work first

--- djangotutorial/mysite/urls.py
+++ djangotutorial/mysite/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('', views.homepage, name='home'),
     path('admin/', admin.site.urls),
+    path('about/', views.about, name='about'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
 ]
---
+++ djangotutorial/mysite/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def homepage(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
+
